e008d2224b89db61a9a5afa7d4af64f860d380d8
Polyteh <prevoivan@gmail.com>
2020-02-11 13:23:05 +0200

    add view

---
+++ RacingWeb/Controllers/SuspentionController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Ninject;
+using RacingDTO.Interfaces;
+using RacingWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RacingWeb.Controllers
+{
+    public class SuspentionController : Controller
+    {
+        private readonly ISuspentionService _suspentionService;
+        private readonly IMapper _mapper;
+        [Inject]
+        public SuspentionController(ISuspentionService suspentionService, IMapper mapper)
+        {
+            _suspentionService = suspentionService;
+            _mapper = mapper;
+        }
+        // GET: Suspention
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public async Task<ActionResult> GetSuspentions()
+        {
+            var listDTOSuspection = await _suspentionService.GetAllAsync();
+            var listViewSuspection = _mapper.Map<IEnumerable<SuspentionView>>(listDTOSuspection);
+            return Json(new { data = listViewSuspection }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
