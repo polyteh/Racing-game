3cb27c9f5b298b794c2bcda913e998885484a0b2
Polyteh <prevoivan@gmail.com>
2020-02-12 09:19:11 +0200

    add suspention index'

--- RacingWeb/Models/SuspentionView.cs
+++ RacingWeb/Models/SuspentionView.cs
@@ -12,8 +12,14 @@ namespace RacingWeb.Models
         [Required]
         [Display(Name = "Suspention model name")]
         public string Name { get; set; }
+        [Required]
+        [Range(3, 10, ErrorMessage = "Rigidity coefficient is out of range. Should be 3..10")]
         public int RigidityKoef { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Manufacturer name should be in the range 3..20 characters")]
         public string Manufacurer { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
         ICollection<RacingCarView> RacingCar { get; set; }
     }
